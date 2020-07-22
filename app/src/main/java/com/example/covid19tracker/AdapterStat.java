55d071ef0b579c06c8b1e1f4126f6feda1d9a20c
PKS <pkumarsaha71@gmail.com>
2020-07-23 02:39:20 +0600

    AdapterStat.java

--- app/src/main/java/com/example/covid19tracker/AdapterStat.java
+++ app/src/main/java/com/example/covid19tracker/AdapterStat.java
@@ -2,6 +2,7 @@ package com.example.covid19tracker;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,17 +26,36 @@ public class AdapterStat extends RecyclerView.Adapter<AdapterStat.HoldStat> {
     @NonNull
     @Override
     public HoldStat onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.row_stat,parent,false);
+        return new HoldStat(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HoldStat holder, int position) {
 
+        //get data
+        ModelStat modelStat = startArrayList.get(position);
+        String country = modelStat.getCountry();
+        String totalConfirmed = modelStat.getTotalConfirmed();
+        String totalDeath = modelStat.getTotalDeaths();
+        String totalRecovered = modelStat.getTotalRecovered();
+        String newConfirmed = modelStat.getNewConfirmed();
+        String newDeath = modelStat.getNewDeaths();
+        String newRecover = modelStat.getNewRecovered();
+
+        //set Data
+        holder.countryTV.setText(country);
+        holder.todayDeathTV.setText(totalDeath);
+        holder.todayCaseTV.setText(totalConfirmed);
+        holder.totalrecoveredTV.setText(totalRecovered);
+        holder.recoveredTV.setText(newRecover);
+        holder.deathTV.setText(newDeath);
+        holder.caseTV.setText(newConfirmed);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return startArrayList.size();
     }
 
 
@@ -43,13 +63,14 @@ public class AdapterStat extends RecyclerView.Adapter<AdapterStat.HoldStat> {
     class HoldStat extends RecyclerView.ViewHolder{
 
         //UI views
-        TextView countryTV,caseTV,deathTV,totalrecoveredTV,todayDeathTV,recoveredTV;
+        TextView countryTV,todayCaseTV,caseTV,deathTV,totalrecoveredTV,todayDeathTV,recoveredTV;
 
 
         public HoldStat(@NonNull View itemView) {
             super(itemView);
             countryTV = itemView.findViewById(R.id.countryTV);
             caseTV = itemView.findViewById(R.id.caseTV);
+            todayCaseTV = itemView.findViewById(R.id.todayCaseTV);
             deathTV = itemView.findViewById(R.id.deathTV);
             todayDeathTV = itemView.findViewById(R.id.todayDeathTV);
             recoveredTV = itemView.findViewById(R.id.recoveredTV);
