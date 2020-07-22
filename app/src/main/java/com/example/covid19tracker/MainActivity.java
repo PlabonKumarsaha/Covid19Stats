05f0c90eeee006a2c507f5f6f264636a68686cee
PKS <pkumarsaha71@gmail.com>
2020-07-22 17:08:38 +0600

    Home and statastics fragment were added and button click work for them was done

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.covid19tracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private TextView titleTV;
     private ImageButton refreshBtn;
     private BottomNavigationView navigationView;
+    private Fragment homeFragment,statFragment;
+    private Fragment activeFragment;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         titleTV = findViewById(R.id.titleTV);
         refreshBtn = findViewById(R.id.refreshBtn);
         navigationView = findViewById(R.id.navigationView);
+        
+        initFragmentManager();
 
         //refreshButtonclick
         refreshBtn.setOnClickListener(new View.OnClickListener() {
@@ -39,10 +48,51 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     }
 
+//this will be initialized when the app starts. When the app starts , home fragment is the first fragment that will start the whole app
+    private void initFragmentManager() {
+    HomeFragment homeFragment = new HomeFragment();
+    StatasticsFragment statasticsFragment = new StatasticsFragment();
+
+    fragmentManager = getSupportFragmentManager();
+    activeFragment = homeFragment;
+
+    //initilaizing with this fragment
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.add(R.id.frame,homeFragment,"Home_Fragment").commit();
+
+    //keeping another fragment hidden!
+    fragmentTransaction.add(R.id.frame,statasticsFragment,"Statastics_fragment")
+    .hide(statasticsFragment).commit();
+    //commit the transaction
+        fragmentTransaction.commit();
+    }
+
+    private void loadHomeFragment(){
+        titleTV.setText("Home");
+        fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit();
+        activeFragment = homeFragment;
+    }
+
+    private void loadStatasticsFragment(){
+        titleTV.setText("Covid 19 statastics");
+        fragmentManager.beginTransaction().hide(activeFragment).show(statFragment).commit();
+        activeFragment = statFragment;
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
         //handles bottom navigation item clicks
+        switch(item.getItemId()){
+
+            case R.id.navigation_home :
+                loadHomeFragment();
+                 return true;
+            case R.id.navigation_stats :
+                loadStatasticsFragment();
+                return true;
+
+        }
         return false;
     }
 }
---
+++ app/src/main/java/com/example/covid19tracker/StatasticsFragment.java
@@ -0,0 +1,34 @@
+package com.example.covid19tracker;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StatasticsFragment extends Fragment {
+
+    Context context;
+    public StatasticsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_statastics, container, false);
+        return view;
+    }
+}
