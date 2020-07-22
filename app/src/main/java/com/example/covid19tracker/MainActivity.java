add7cac57a742c46e4f84f59f9d24a82bfc9fe82
PKS <pkumarsaha71@gmail.com>
2020-07-22 12:43:58 +0600

    UI were added

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.covid19tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private TextView titleTV;
+    private ImageButton refreshBtn;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titleTV = findViewById(R.id.titleTV);
+        refreshBtn = findViewById(R.id.refreshBtn);
+        navigationView = findViewById(R.id.navigationView);
+
+        //refreshButtonclick
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        //handles bottom navigation item clicks
+        return false;
     }
 }
