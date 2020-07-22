175d88564c330330a1bef1155567715d11914ae6
PKS <pkumarsaha71@gmail.com>
2020-07-22 23:03:07 +0600

    ModelStat.java was made

---
+++ app/src/main/java/com/example/covid19tracker/AdapterStat.java
@@ -0,0 +1,17 @@
+package com.example.covid19tracker;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterStat {
+    //view hold class
+    class HoldStat extends RecyclerView.ViewHolder{
+
+        public HoldStat(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    
+}
---
+++ app/src/main/java/com/example/covid19tracker/ModelStat.java
@@ -0,0 +1,116 @@
+package com.example.covid19tracker;
+
+public class ModelStat {
+ //use same variable name as  in JSON response
+    /*
+    "Country": "Albania",
+            "CountryCode": "AL",
+            "Slug": "albania",
+            "NewConfirmed": 119,
+            "TotalConfirmed": 4290,
+            "NewDeaths": 4,
+            "TotalDeaths": 117,
+            "NewRecovered": 45,
+            "TotalRecovered": 2397,
+            "Date": "2020-07-22T12:53:41Z",
+            "Premium": {}
+     */
+    String Country,CountryCode,Slug,NewConfirmed,TotalConfirmed,NewDeaths,TotalDeaths,NewRecovered,TotalRecovered,Date;
+    public ModelStat(){
+
+    }
+    public ModelStat(String country, String countryCode, String slug, String newConfirmed, String totalConfirmed
+                    , String newDeaths, String totalDeaths, String newRecovered, String totalRecovered, String date) {
+        Country = country;
+        CountryCode = countryCode;
+        Slug = slug;
+        NewConfirmed = newConfirmed;
+        TotalConfirmed = totalConfirmed;
+        NewDeaths = newDeaths;
+        TotalDeaths = totalDeaths;
+        NewRecovered = newRecovered;
+        TotalRecovered = totalRecovered;
+        Date = date;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        CountryCode = countryCode;
+    }
+
+    public String getSlug() {
+        return Slug;
+    }
+
+    public void setSlug(String slug) {
+        Slug = slug;
+    }
+
+    public String getNewConfirmed() {
+        return NewConfirmed;
+    }
+
+    public void setNewConfirmed(String newConfirmed) {
+        NewConfirmed = newConfirmed;
+    }
+
+    public String getTotalConfirmed() {
+        return TotalConfirmed;
+    }
+
+    public void setTotalConfirmed(String totalConfirmed) {
+        TotalConfirmed = totalConfirmed;
+    }
+
+    public String getNewDeaths() {
+        return NewDeaths;
+    }
+
+    public void setNewDeaths(String newDeaths) {
+        NewDeaths = newDeaths;
+    }
+
+    public String getTotalDeaths() {
+        return TotalDeaths;
+    }
+
+    public void setTotalDeaths(String totalDeaths) {
+        TotalDeaths = totalDeaths;
+    }
+
+    public String getNewRecovered() {
+        return NewRecovered;
+    }
+
+    public void setNewRecovered(String newRecovered) {
+        NewRecovered = newRecovered;
+    }
+
+    public String getTotalRecovered() {
+        return TotalRecovered;
+    }
+
+    public void setTotalRecovered(String totalRecovered) {
+        TotalRecovered = totalRecovered;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+}
